b24b523b0562e8af715283a72907ae6a3b2deebf
Vladimir Drobnjak <vladimirdrobnjak@192.168.0.103>
2025-07-16 02:16:33 +0200

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -93,10 +93,10 @@ export default function FixedContainer() {
           <Box
             sx={{
               width: "100vw",
-              marginLeft:"-100px",
+              marginLeft: { xs: "-30", md: "-100px" },
               height: "80px",
               marginTop: "100px",
-              background: "linear-gradient(180deg, rgba(15, 95, 174, 0) 0%, rgba(42, 40, 40, 0.8) 50%, rgba(15, 95, 174, 0) 100%)",
+              background: "linear-gradient(180deg, rgba(15, 95, 174, 0) 8%, rgba(42, 40, 40, 0.8) 50%, rgba(15, 95, 174, 0) 92%)",
 
               backdropFilter: "blur(2px)",
               WebkitBackdropFilter: "blur(10px)",
